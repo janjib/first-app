a13088067e6a75081c7fa4b483e6470267067a07
janjib <janjib04@gmail.com>
2021-07-31 22:47:43 +0800

    First deployment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
+import Homepage from './components/Homepage';
+import Secondpage from './components/Secondpage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+           <Route path='/first-app' exact component={Homepage} />
+           <Route path='/first-app/secondpage' exact component={Secondpage}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
